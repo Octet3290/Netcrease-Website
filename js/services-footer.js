f2b1ec1a4bb8bf85ba686c031fbd6611a7c5da8a
rahul <rahul@netcrease.com>
2026-01-07 15:11:57 +0530

    first commit

---
+++ js/services-footer.js
@@ -0,0 +1,8 @@
+
+fetch("../components/services-footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+    
+  });
+
---
+++ js/smooth-scroll.js
@@ -0,0 +1,32 @@
+// Initialize Lenis smooth scrolling
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    direction: 'vertical',
+    gestureDirection: 'vertical',
+    smooth: true,
+    mouseMultiplier: 1,
+    smoothTouch: false,
+    touchMultiplier: 2,
+    infinite: false,
+});
+
+// Update ScrollTrigger to use Lenis
+lenis.on('scroll', ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
+
+// Anchor link smooth scroll integration
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         const target = document.querySelector(this.getAttribute('href'));
+//         if (target) {
+//             lenis.scrollTo(target);
+//         }
+//     });
+// });
